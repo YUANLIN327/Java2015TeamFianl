18ae9f36f33a340c5604519156999467dfc2fe1a
YUANLIN327 <yuanlin.dalian@gmail.com>
2015-11-23 21:35:26 -0600

    Lin:made change

--- FinalProject/src/CoffeePOS.java
+++ FinalProject/src/CoffeePOS.java
@@ -29,6 +29,7 @@ public class CoffeePOS extends JFrame {
 
 	//Sam made the first change
 	//Claudia made a change
+	//Lin made the change
 	private JPanel contentPane;
 	BufferedImage buttonIcon1 = ImageIO.read(new File("TeaButton.jpg"));	
 	BufferedImage buttonIcon2 = ImageIO.read(new File("LatteButton.jpg"));
