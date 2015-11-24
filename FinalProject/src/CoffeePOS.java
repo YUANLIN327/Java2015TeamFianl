76adf4ad745eacd354620e0736f7208399082aee
claudiazamudio <claudia_zamudio@baylor.edu>
2015-11-23 21:34:10 -0600

    Claudia: first change

--- FinalProject/src/CoffeePOS.java
+++ FinalProject/src/CoffeePOS.java
@@ -28,6 +28,7 @@ import javax.swing.SwingConstants;
 public class CoffeePOS extends JFrame {
 
 	//Sam made the first change
+	//Claudia made a change
 	private JPanel contentPane;
 	BufferedImage buttonIcon1 = ImageIO.read(new File("TeaButton.jpg"));	
 	BufferedImage buttonIcon2 = ImageIO.read(new File("LatteButton.jpg"));
